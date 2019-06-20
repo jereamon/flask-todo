3dc1ef3d28865f2cf9fff4c68b137038f999ddd6
Jeremy <jereamon@gmail.com>
2019-06-20 13:23:53 -0700

    initial commit

---
+++ model.py
@@ -0,0 +1,21 @@
+from peewee import Model, CharField, DateTimeField, ForeignKeyField
+import os
+
+from playhouse.db_url import connect
+
+db = connect(os.environ.get('DATABASE_URL', 'sqlite:///my_database.db'))
+
+
+class User(Model):
+    name = CharField(unique=True, max_length=44)
+    password = CharField(max_length=44)
+    class Meta:
+        database = db
+
+
+class Task(Model):
+    name = CharField(max_length=30)
+    performed = DateTimeField(null=True)
+    performed_by = ForeignKeyField(model=User, null=True)
+    class Meta:
+        database = db
